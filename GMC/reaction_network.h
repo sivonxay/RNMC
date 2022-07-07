9caa81b2fd98292d0dad511fb1b4e50bbbf2333a
Eric Sivonxay <sivonxay@gmail.com>
2022-07-07 16:57:59 -0700

    Revert "Remove GMC"
    This reverts commit dc6226fed9703bb6e3919bc4d56ab68ee14995a8.

---
+++ GMC/reaction_network.h
@@ -0,0 +1,301 @@
+#pragma once
+#include <stdint.h>
+#include <vector>
+#include <optional>
+#include <mutex>
+#include "../core/sql.h"
+#include "sql_types.h"
+#include "../core/solvers.h"
+#include "../core/simulation.h"
+
+struct Reaction {
+    // we assume that each reaction has zero, one or two reactants
+    uint8_t number_of_reactants;
+    uint8_t number_of_products;
+
+    int reactants[2];
+    int products[2];
+
+    double rate;
+};
+
+
+
+// parameters passed to the ReactionNetwork constructor
+// by the dispatcher which are model specific
+struct ReactionNetworkParameters {
+};
+
+
+struct ReactionNetwork {
+    std::vector<Reaction> reactions; // list of reactions
+    std::vector<int> initial_state; // initial state for all the simulations
+    std::vector<double> initial_propensities; // initial propensities for all the reactions
+    double factor_zero; // rate modifer for reactions with zero reactants
+    double factor_two; // rate modifier for reactions with two reactants
+    double factor_duplicate; // rate modifier for reactions of form A + A -> ...
+
+    // maps species to the reactions which involve that species
+    std::vector<std::vector<int>> dependents;
+
+    ReactionNetwork(
+        SqlConnection &reaction_network_database,
+        SqlConnection &initial_state_database,
+        ReactionNetworkParameters parameters);
+
+    void compute_dependents();
+
+    double compute_propensity(
+        std::vector<int> &state,
+        int reaction_index);
+
+    void update_state(
+        std::vector<int> &state,
+        int reaction_index);
+
+    void update_propensities(
+        std::function<void(Update update)> update_function,
+        std::vector<int> &state,
+        int next_reaction
+        );
+
+    // convert a history element as found a simulation to history
+    // to a SQL type.
+    WriteTrajectoriesSql history_element_to_sql(
+        int seed,
+        HistoryElement history_element);
+
+};
+
+ReactionNetwork::ReactionNetwork(
+     SqlConnection &reaction_network_database,
+     SqlConnection &initial_state_database,
+     ReactionNetworkParameters)
+
+    {
+
+    // collecting reaction network metadata
+    SqlStatement<MetadataSql> metadata_statement (reaction_network_database);
+    SqlReader<MetadataSql> metadata_reader (metadata_statement);
+
+    std::optional<MetadataSql> maybe_metadata_row = metadata_reader.next();
+
+    if (! maybe_metadata_row.has_value()) {
+        std::cerr << time_stamp()
+                  << "no metadata row\n";
+
+        std::abort();
+    }
+
+    MetadataSql metadata_row = maybe_metadata_row.value();
+
+
+
+    // setting reaction network factors
+    SqlStatement<FactorsSql> factors_statement (initial_state_database);
+    SqlReader<FactorsSql> factors_reader (factors_statement);
+
+    // TODO: make sure this isn't nothing
+    FactorsSql factors_row = factors_reader.next().value();
+    factor_zero = factors_row.factor_zero;
+    factor_two = factors_row.factor_two;
+    factor_duplicate = factors_row.factor_duplicate;
+
+    // loading intial state
+    initial_state.resize(metadata_row.number_of_species);
+
+    SqlStatement<InitialStateSql> initial_state_statement (initial_state_database);
+    SqlReader<InitialStateSql> initial_state_reader (initial_state_statement);
+
+    int species_id;
+    while(std::optional<InitialStateSql> maybe_initial_state_row =
+          initial_state_reader.next()) {
+
+        InitialStateSql initial_state_row = maybe_initial_state_row.value();
+        species_id = initial_state_row.species_id;
+        initial_state[species_id] = initial_state_row.count;
+    }
+
+    // loading reactions
+    // vectors are default initialized to empty.
+    // it is "cleaner" to resize the default vector than to
+    // drop it and reinitialize a new vector.
+    reactions.resize(metadata_row.number_of_reactions);
+
+    SqlStatement<ReactionSql> reaction_statement (reaction_network_database);
+    SqlReader<ReactionSql> reaction_reader (reaction_statement);
+
+
+    // reaction_id is lifted so we can do a sanity check after the
+    // loop.  Make sure size of reactions vector, last reaction_id and
+    // metadata number_of_reactions are all the same
+
+    unsigned long int reaction_id = 0;
+
+    while(std::optional<ReactionSql> maybe_reaction_row = reaction_reader.next()) {
+
+        ReactionSql reaction_row = maybe_reaction_row.value();
+        uint8_t number_of_reactants = reaction_row.number_of_reactants;
+        uint8_t number_of_products = reaction_row.number_of_products;
+        reaction_id = reaction_row.reaction_id;
+
+
+        Reaction reaction = {
+            .number_of_reactants = number_of_reactants,
+            .number_of_products = number_of_products,
+            .reactants = { reaction_row.reactant_1, reaction_row.reactant_2 },
+            .products = { reaction_row.product_1, reaction_row.product_2},
+            .rate = reaction_row.rate
+        };
+
+        reactions[reaction_id] = reaction;
+
+        }
+
+    // sanity check
+    if ( metadata_row.number_of_reactions != reaction_id + 1 ||
+         metadata_row.number_of_reactions != reactions.size() ) {
+        // TODO: improve logging
+        std::cerr << time_stamp() <<  "reaction loading failed\n";
+        std::abort();
+    }
+
+    // computing initial propensities
+    initial_propensities.resize(metadata_row.number_of_reactions);
+    for (unsigned long int i = 0; i < initial_propensities.size(); i++) {
+        initial_propensities[i] = compute_propensity(initial_state, i);
+    }
+
+    std::cerr << time_stamp() << "computing dependency graph...\n";
+    compute_dependents();
+    std::cerr << time_stamp() << "finished computing dependency graph\n";
+};
+
+
+void ReactionNetwork::compute_dependents() {
+    // initializing dependency graph
+
+    dependents.resize(initial_state.size());
+
+
+    for ( unsigned int reaction_id = 0; reaction_id <  reactions.size(); reaction_id++ ) {
+        Reaction &reaction = reactions[reaction_id];
+
+        for ( int i = 0; i < reaction.number_of_reactants; i++ ) {
+            int reactant_id = reaction.reactants[i];
+            dependents[reactant_id].push_back(reaction_id);
+        }
+
+
+        for ( int j = 0; j < reaction.number_of_products; j++ ) {
+            int product_id = reaction.products[j];
+            dependents[product_id].push_back(reaction_id);
+        }
+    }
+
+};
+
+
+double ReactionNetwork::compute_propensity(
+    std::vector<int> &state,
+    int reaction_index) {
+
+    Reaction &reaction = reactions[reaction_index];
+
+    double p;
+    // zero reactants
+    if (reaction.number_of_reactants == 0)
+        p = factor_zero * reaction.rate;
+
+    // one reactant
+    else if (reaction.number_of_reactants == 1)
+        p = state[reaction.reactants[0]] * reaction.rate;
+
+
+    // two reactants
+    else {
+        if (reaction.reactants[0] == reaction.reactants[1])
+            p = factor_duplicate
+                * factor_two
+                * state[reaction.reactants[0]]
+                * (state[reaction.reactants[0]] - 1)
+                * reaction.rate;
+
+        else
+            p = factor_two
+                * state[reaction.reactants[0]]
+                * state[reaction.reactants[1]]
+                * reaction.rate;
+    }
+
+    return p;
+
+};
+
+void ReactionNetwork::update_state(
+    std::vector<int> &state,
+    int reaction_index) {
+
+    for (int m = 0;
+         m < reactions[reaction_index].number_of_reactants;
+         m++) {
+        state[reactions[reaction_index].reactants[m]]--;
+    }
+
+    for (int m = 0;
+         m < reactions[reaction_index].number_of_products;
+         m++) {
+        state[reactions[reaction_index].products[m]]++;
+    }
+
+}
+
+
+void ReactionNetwork::update_propensities(
+    std::function<void(Update update)> update_function,
+    std::vector<int> &state,
+    int next_reaction
+    ) {
+
+    Reaction &reaction = reactions[next_reaction];
+
+    std::vector<int> species_of_interest;
+    species_of_interest.reserve(4);
+
+    for ( int i = 0; i < reaction.number_of_reactants; i++ ) {
+        int reactant_id = reaction.reactants[i];
+        species_of_interest.push_back(reactant_id);
+    }
+
+
+    for ( int j = 0; j < reaction.number_of_products; j++ ) {
+        int product_id = reaction.products[j];
+        species_of_interest.push_back(product_id);
+    }
+
+
+    for ( int species_id : species_of_interest ) {
+        for ( unsigned int reaction_index : dependents[species_id] ) {
+
+            double new_propensity = compute_propensity(
+                state,
+                reaction_index);
+
+            update_function(Update {
+                    .index = reaction_index,
+                    .propensity = new_propensity});
+        }
+    }
+}
+
+
+WriteTrajectoriesSql ReactionNetwork::history_element_to_sql(
+    int seed,
+    HistoryElement history_element) {
+    return WriteTrajectoriesSql {
+        .seed = seed,
+        .step = history_element.step,
+        .reaction_id = history_element.reaction_id,
+        .time = history_element.time
+    };
+}
---
+++ GMC/sql_types.h
@@ -0,0 +1,103 @@
+#pragma once
+#include <sqlite3.h>
+#include <string>
+
+struct MetadataSql {
+    unsigned long int number_of_species;
+    unsigned long int number_of_reactions;
+    static std::string sql_statement;
+    static void action(MetadataSql &r, sqlite3_stmt *stmt);
+};
+
+std::string MetadataSql::sql_statement =
+    "SELECT number_of_species, number_of_reactions FROM metadata;";
+
+void MetadataSql::action(MetadataSql &r, sqlite3_stmt *stmt) {
+        r.number_of_species = sqlite3_column_int(stmt, 0);
+        r.number_of_reactions = sqlite3_column_int(stmt, 1);
+};
+
+struct ReactionSql {
+    unsigned long int reaction_id;
+    int number_of_reactants;
+    int number_of_products;
+    int reactant_1;
+    int reactant_2;
+    int product_1;
+    int product_2;
+    double rate;
+    static std::string sql_statement;
+    static void action(ReactionSql &r, sqlite3_stmt *stmt);
+};
+
+std::string ReactionSql::sql_statement =
+    "SELECT reaction_id, number_of_reactants, number_of_products, "
+    "reactant_1, reactant_2, product_1, product_2, rate FROM reactions;";
+
+
+void ReactionSql::action(ReactionSql &r, sqlite3_stmt *stmt) {
+        r.reaction_id = sqlite3_column_int(stmt, 0);
+        r.number_of_reactants = sqlite3_column_int(stmt, 1);
+        r.number_of_products = sqlite3_column_int(stmt, 2);
+        r.reactant_1 = sqlite3_column_int(stmt, 3);
+        r.reactant_2 = sqlite3_column_int(stmt, 4);
+        r.product_1 = sqlite3_column_int(stmt, 5);
+        r.product_2 = sqlite3_column_int(stmt, 6);
+        r.rate = sqlite3_column_double(stmt, 7);
+};
+
+
+struct InitialStateSql {
+    int species_id;
+    int count;
+    static std::string sql_statement;
+    static void action(InitialStateSql &r, sqlite3_stmt *stmt);
+};
+
+std::string InitialStateSql::sql_statement =
+    "SELECT species_id, count FROM initial_state;";
+
+void InitialStateSql::action(InitialStateSql &r, sqlite3_stmt *stmt) {
+    r.species_id = sqlite3_column_int(stmt, 0);
+    r.count = sqlite3_column_int(stmt, 1);
+}
+
+struct WriteTrajectoriesSql {
+    int seed;
+    int step;
+    int reaction_id;
+    double time;
+    static std::string sql_statement;
+    static void action(WriteTrajectoriesSql &r, sqlite3_stmt *stmt);
+};
+
+std::string WriteTrajectoriesSql::sql_statement =
+    "INSERT INTO trajectories VALUES (?1, ?2, ?3, ?4);";
+
+void WriteTrajectoriesSql::action (WriteTrajectoriesSql& t, sqlite3_stmt* stmt) {
+    sqlite3_bind_int(stmt, 1, t.seed);
+    sqlite3_bind_int(stmt, 2, t.step);
+    sqlite3_bind_int(stmt, 3, t.reaction_id);
+    sqlite3_bind_double(stmt, 4, t.time);
+};
+
+
+struct FactorsSql {
+    double factor_zero;
+    double factor_two;
+    double factor_duplicate;
+    static std::string sql_statement;
+    static void action(FactorsSql &r, sqlite3_stmt *stmt);
+};
+
+
+std::string FactorsSql::sql_statement =
+    "SELECT factor_zero, factor_two, factor_duplicate FROM factors";
+
+
+void FactorsSql::action (FactorsSql &r, sqlite3_stmt *stmt) {
+    r.factor_zero = sqlite3_column_double(stmt, 0);
+    r.factor_two = sqlite3_column_double(stmt, 1);
+    r.factor_duplicate = sqlite3_column_double(stmt, 2);
+};
+
